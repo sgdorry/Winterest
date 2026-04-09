bf955289e17b22fd255904381b53e1bb4ba3fd5a
3btesham <ebtesham90@gmail.com>
2026-04-09 00:27:36 -0400

    fixing reverts

--- states/queries_states.py
+++ states/queries_states.py
@@ -157,21 +157,6 @@ def create(fields: dict):
 
     if not fields.get(AREA) or not isinstance(fields[AREA], str):
         raise ValueError(f'Bad value for {fields.get(AREA)=}')
-<<<<<<< HEAD
-
-    if not fields.get(CODE) or not isinstance(fields[CODE], str):
-        raise ValueError(f'Bad value for {fields.get(CODE)=}')
-
-    if not fields.get(GOVERNOR) or not isinstance(fields[GOVERNOR], str):
-        raise ValueError(f'Bad value for {fields.get(GOVERNOR)=}')
-
-    if not fields.get(CLIMATE) or not isinstance(fields[CLIMATE], str):
-        raise ValueError(f'Bad value for {fields.get(CLIMATE)=}')
-
-    if not fields.get(STATE_BIRD) or not isinstance(fields[STATE_BIRD], str):
-        raise ValueError(f'Bad value for {fields.get(STATE_BIRD)=}')
-=======
->>>>>>> parent of 78020a5 (fixed state query functions, incorrect state data labelling in load)
 
     if (not fields.get(STATEHOOD_DATE)
             or not isinstance(fields[STATEHOOD_DATE], str)):
@@ -219,13 +204,6 @@ def update(state_id: str, fields: dict):
     if GDP in fields:
         if not fields[GDP] or not isinstance(fields[GDP], str):
             raise ValueError(f'Bad value for {fields.get(GDP)=}')
-<<<<<<< HEAD
-
-    if CLIMATE in fields:
-        if not fields[CLIMATE] or not isinstance(fields[CLIMATE], str):
-            raise ValueError(f'Bad value for {fields.get(CLIMATE)=}')
-=======
->>>>>>> parent of 78020a5 (fixed state query functions, incorrect state data labelling in load)
 
     if (not fields[STATEHOOD_DATE]
             or not isinstance(fields[STATEHOOD_DATE], str)):
